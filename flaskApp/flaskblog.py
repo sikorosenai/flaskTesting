fe79a1fd5e0a501835fd480d84a5d6c86fa94861
Silas Maughan <silas.maughan@gmail.com>
2022-12-12 20:08:54 +0100

    Setting up boilerplate and structure of project

--- flaskApp/flaskblog.py
+++ flaskApp/flaskblog.py
@@ -1,10 +1,25 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Silas Blog</p>"
+dummy = [
+    {
+        'title': 'Blog post 1',
+        'author': 'Sikorosenai',
+        'content': 'First post content',
+        'date': 'December 12, 2022'
+    }
+]
+
+# All return home
+@app.route('/home')
+@app.route('/')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', post=dummy)
 
 if __name__ == '__main__':
     app.run(debug=True)
