01975658350158db2c25a90f8466241ae0db1e24
Silas Maughan <silas.maughan@gmail.com>
2022-12-15 21:49:44 +0100

    Added login and create account functionality

--- flaskApp/flaskblog.py
+++ flaskApp/flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -25,14 +25,23 @@ def about():
 def posts():
     return render_template('posts.html', post=dummy)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST']) 
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!')
+        return redirect(url_for('about'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'admin' and form.password.data == 'password':
+            flash(f'User account {form.username.data} has now logged in!')
+            return redirect(url_for('posts'))
+        else:
+            flash('Login Unsuccessful. Please check username and password')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
